3333409bd8d5b57adce7694b245a39c928163f4e
Daniel Amme <blackmystic360@gmail.com>
2023-09-03 23:52:21 +0200

    Next Level

--- uber_pickups.py
+++ uber_pickups.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Uber pickups in NYC by Daniel')
+st.title('Uber pickups in NYC')
 
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
@@ -16,22 +16,60 @@ def load_data(nrows):
     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     return data
 
-# Create a text element and let the reader know the data is loading.
-data_load_state = st.text('Loading data...')
-# Load 10,000 rows of data into the dataframe.
-data = load_data(10000)
-# Notify the reader that the data was successfully loaded.
-data_load_state.text("Done! (using st.cache_data)")
+tab1, tab2, tab3 = st.tabs(["Tab 1", "Tab2","Tab3"])
+tab1.write("this is tab 1")
+with tab1:
+    # Create a text element and let the reader know the data is loading.
+    data_load_state = st.text('Loading data...')
+    # Load 10,000 rows of data into the dataframe.
+    data = load_data(1000000)
+    # Notify the reader that the data was successfully loaded.
+    data_load_state.text("Done! (using st.cache_data)")
 
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(data)
-st.subheader('Number of pickups by hour')
-hist_values = np.histogram(
-    data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
+    if st.checkbox('Show raw data'):
+        st.subheader('Raw data')
+        st.write(data)
+    st.subheader('Number of pickups by hour')
+    hist_values = np.histogram(
+        data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+    st.bar_chart(hist_values)
 
-hour_to_filter = st.slider('hour', 0, 23, 17)  # min: 0h, max: 23h, default: 17h
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-st.subheader(f'Map of all pickups at {hour_to_filter}:00')
-st.map(filtered_data)
+    hour_to_filter = st.slider('hour', 0, 23, 17)  # min: 0h, max: 23h, default: 17h
+    filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+    st.subheader(f'Map of all pickups at {hour_to_filter}:00')
+    st.map(filtered_data)
+
+tab2.write("this is tab 2")
+with tab2:
+    # Just add it after st.sidebar:
+    #st.sidebar:
+    a = st.sidebar.radio('Choose:',[1,2])
+    st.button('Hit me')
+    #st.data_editor('Edit data', data)
+    st.checkbox('Check me out')
+    st.radio('Pick one:', ['nose','ear'])
+    st.selectbox('Select', [1,2,3])
+    st.multiselect('Multiselect', [1,2,3])
+    st.slider('Slide me', min_value=0, max_value=10)
+    st.select_slider('Slide to select', options=[1,'2'])
+    st.text_input('Enter some text')
+    st.number_input('Enter a number')
+    st.text_area('Area for textual entry')
+    st.date_input('Date input')
+    st.time_input('Time entry')
+    st.file_uploader('File uploader')
+    #st.download_button('On the dl', data)
+    st.camera_input("一二三,茄子!")
+    st.color_picker('Pick a color')
+
+tab3.write("This is Tab3")
+with tab3:
+    st.balloons()
+    st.snow()
+    st.toast('Mr Stay-Puft')
+    st.error('Error message')
+    st.warning('Warning message')
+    st.info('Info message')
+    st.success('Success message')
+    st.exception('error message')
+    
